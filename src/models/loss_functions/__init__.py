d5f2de220b3da01ecf2df1df80d0463cce4e32fb
Jonas Keller <jonaskeller2013@t-online.de>
2026-06-16 08:04:52 +0200

    fix: allow radially weighted loss to be loaded from model file

---
+++ src/models/loss_functions/__init__.py
@@ -0,0 +1,4 @@
+from .centerFocusedTverskyLoss import CenterFocusedTverskyLoss
+from .radiallyWeightedLoss import RadiallyWeightedLoss
+
+LOSS_FUNCTIONS = [CenterFocusedTverskyLoss, RadiallyWeightedLoss]
